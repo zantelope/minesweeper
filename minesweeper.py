3a7d3b7f3210211f80e56bffe27c810db3b93c85
zantelope <42042206+zantelope@users.noreply.github.com>
2018-10-07 16:54:24 -0500

    Update minesweeper.py

--- minesweeper.py
+++ minesweeper.py
@@ -1,8 +1,21 @@
+#possibly ok?
 def minesweeper(matrix):
+  adjacentMines = []
+  finalMatrix = []
+  matrixB = padMatrix(matrix)
+  print (matrixB)
+  for i in range(1, len(matrixB) - 1):
+    for j in range(1, len(matrixB[i]) - 1):
+      sumAdjacentMines = matrixB[i - 1][j - 1] + matrixB[i - 1][j] + matrixB[i - 1][j + 1] + matrixB[i][j - 1] + matrixB[i][j + 1] + matrixB[i + 1][j - 1] + matrixB[i + 1][j] + matrixB[i + 1][j + 1]
+      adjacentMines.append(sumAdjacentMines)
+      
 
 
+  for i in range(0, len(adjacentMines), len(matrix[0]) - 2):
+    finalMatrix.append(adjacentMines[i: i + len(matrix[0]) - 2])
+  return finalMatrix
 
-
+#must fix, this makes no sense and doesn't work
 def padMatrix(matrix):
   matrix.insert(0, [])
   for i in range(len(matrix[1])):
@@ -14,3 +27,4 @@ def padMatrix(matrix):
     matrix[i].insert(0, False)
     matrix[i].append(False)
   return matrix
+
